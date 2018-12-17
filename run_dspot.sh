06c6028f9d2b9310c0cecf9fa9ce93a24e8feb97
Nicola Bertazzo <nicola.bertazzo@eng.it>
2018-12-17 17:48:55 +0100

    fix package name of JUnit Class in sh file

--- run_dspot.sh
+++ run_dspot.sh
@@ -8,6 +8,6 @@ dspotPropertiesFile="dhell.dspot"
 
 traceFile="dspot.traces"
 
-echo "java -jar $dspotJar --path-to-properties $dspotPropertiesFile -i 1 --test eu.stampproject.examples.dhell.HelloAppTest -a MethodAdd --verbose" 2>&1 | tee $traceFile
+echo "java -jar $dspotJar --path-to-properties $dspotPropertiesFile -i 1 --test eu.stamp_project.examples.dhell.HelloAppTest -a MethodAdd --verbose" 2>&1 | tee $traceFile
 echo "--------------------------------------------------------------------------------" >>  $traceFile
-java -jar $dspotJar --path-to-properties $dspotPropertiesFile -i 1 --test eu.stampproject.examples.dhell.HelloAppTest -a MethodAdd --verbose 2>&1 | tee -a $traceFile
+java -jar $dspotJar --path-to-properties $dspotPropertiesFile -i 1 --test eu.stamp_project.examples.dhell.HelloAppTest -a MethodAdd --verbose 2>&1 | tee -a $traceFile
